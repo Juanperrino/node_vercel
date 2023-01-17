d34a3f3977e95da36e3ffacd4904e035b77e01e9
Juan Perrino <juanperrino90@gmail.com>
2023-01-17 15:55:22 -0300

    despliegue vercel

---
+++ routes/login.js
@@ -0,0 +1,66 @@
+import { Router } from "express";
+import passport, { Passport } from "passport";
+import { logger } from "../log/logger.js";
+
+const router = Router();
+
+const auth = (req, res, next) => {
+    if (req.isAuthenticated()) {
+        next()
+    } else {
+        res.redirect("/login")
+    }
+}
+
+router.get("/", auth, (req, res) => {
+    logger.info(`Se accedio a la ruta ${req.originalUrl} con el metodo ${req.method}`)
+    const { user } = req;
+    const data = { username: user.email };
+    res.render("index", data);
+});
+
+router.get("/register", (req, res) => {
+    logger.info(`Se accedio a la ruta ${req.originalUrl} con el metodo ${req.method}`)
+    if (req.isAuthenticated())
+        return res.redirect("/");
+    res.render("register");
+})
+
+router.post("/register", passport.authenticate("register", { failureRedirect: "/error", failureMessage: true }), (req, res) => {
+    logger.info(`Se accedio a la ruta ${req.originalUrl} con el metodo ${req.method}`)
+    res.redirect("/");
+});
+
+router.get("/login", (req, res) => {
+    logger.info(`Se accedio a la ruta ${req.originalUrl} con el metodo ${req.method}`)
+    if (req.isAuthenticated())
+        return res.redirect("/");
+    res.render("login");
+})
+
+router.post("/login", passport.authenticate("login", { failureRedirect: "/error", failureMessage: true }), (req, res) => {
+    logger.info(`Se accedio a la ruta ${req.originalUrl} con el metodo ${req.method}`)
+    res.redirect("/");
+});
+
+router.get("/error", (req, res, next) => {
+    logger.info(`Se accedio a la ruta ${req.originalUrl} con el metodo ${req.method}`)
+    console.log(req.session.messages)
+    res.render("error", { message: req.session.messages[req.session.messages.length - 1] })
+})
+
+router.get("/logout", (req, res, next) => {
+    logger.info(`Se accedio a la ruta ${req.originalUrl} con el metodo ${req.method}`)
+    const { user } = req;
+    req.logout((error) => {
+        if (error) {
+            logger.error(error.message)
+            return next(error);
+        }
+        let data = { username: user.email };
+        res.render("logout", data);
+
+    })
+});
+
+export default router;
---
+++ routes/products.js
@@ -0,0 +1,52 @@
+import { Router } from 'express';
+import { faker } from '@faker-js/faker';
+import { logger } from '../log/logger.js';
+
+faker.locale = 'es';
+const { commerce, image } = faker;
+
+const router = Router();
+
+router.get('/productos-test', (req, res, next) => {
+      try {
+            logger.info(`Se accedio a la ruta ${req.originalUrl} con el metodo ${req.method}`)
+            let data = { productos: [] };
+
+            for (let i = 0; i < 5; i++) {
+                  data.productos.push({
+                        nombre: commerce.product(),
+                        precio: commerce.price(),
+                        url: image.technics(),
+                  });
+            }
+            console.log(data);
+            res.render("productos", data);
+      } catch (error) {
+            logger.error(`${error.message}`)
+            next(error);
+      }
+});
+
+router.get("/randoms", (req, res) => {
+
+      logger.info(`Se accedio a la ruta ${req.originalUrl} con el metodo ${req.method}`)
+      const { cant = 1000000 } = req.query;
+      if (isNaN(Number(cant))) {
+            logger.error(`El numero ingresado es un string`)
+            res.json({ error: "El numero ingresado es un string" })
+      } else {
+            console.log(cant)
+            const child = fork("calculo.js");
+
+            child.on("message", (result) => {
+                  if (result == "ready") {
+                        child.send(Number(cant))
+                  } else {
+                        console.log("llegue aqui")
+                        res.json(result)
+                  }
+            })
+      }
+});
+
+export default router;
